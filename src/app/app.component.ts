f84c4f0b5e9b647737b9eae4fc9f5d2eaabffff6
Albin.Kurian <Albin.Kurian@ACER1215>
2023-10-10 19:55:52 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,56 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'formApp';
+  displayName='';
+  displayName1='';
+  countV=0
+  getValue(val: string){
+    this.displayName=val
+  }
+  getValue1(val1: string){
+    this.displayName1=val1  
+  }
+
+
+  counter(type:string){
+    type=='plus' ? this.countV++ : this.countV--
+  }
+
+  isDisabled1=false
+  isDisabled2=false
+
+
+  nameofuser='albin'
+  nameofuser2='sebana'
+
+
+  show ='yes'
+
+  changeShow(){
+    this.show=this.show=='yes' ? 'no' : 'yes'
+  }
+
+  color ='green'
+
+  switchColor = 'blue'
+
+  users =['Albin','Alen','joshit','Sebana','akash']
+  userDetails =[
+    { name:'Albin',email : 'Albin@gmail.com'},
+    {name : 'sebana' , email : 'sebana@gmail.com'},
+    {name : 'kurian',email:'kurian@gmail.com'},
+    {name: 'Albinseb',email:'Albinseb@gmail.com'}
+
+    
+
+  ]
+  userDetails1 =[
+    { name:'Albin',email : 'Albin@gmail.com',social:['fb','insta','snapchat','linkedIn','whatsapp']},
+    {name : 'sebana' , email : 'sebana@gmail.com', social:['fb','insta','snapchat']},
+    {name : 'kurian',email:'kurian@gmail.com',social:['fb','insta','whatsapp']},
+    {name: 'Albinseb',email:'Albinseb@gmail.com',social:['linkedIn','whatsapp']}
+
+    
+
+  ]
 }
